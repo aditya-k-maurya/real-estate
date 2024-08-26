529e2a10c29705952eeb18be1f07d5bd741fdfe3
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-08-26 10:45:00 +0530

    register user

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,28 +1,39 @@
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@/lib/prisma";
 
-export async function POST(request:any) {
+export async function POST(request: any) {
 	try {
 		const { username, email, password } = await request.json();
 
-		console.log(username, email, password);
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const newUser = await prisma.user.create({
+			data: {
+				username,
+				email,
+				password: hashedPassword,
+			},
+		});
+
+		console.log(newUser);
 
 		// Return a successful response
 		return NextResponse.json(
 			{
 				success: true,
-				message: "Registration successful",
+				message: "User Registeration Successful",
 			},
 			{
-				
 				status: 200,
 			}
 		);
-	} catch (error:any) {
+	} catch (error: any) {
 		// Return an error response
 		return NextResponse.json(
 			{
 				success: false,
-				message: `Something went wrong: ${error.message}`,
+				message: `Failed to create User: ${error.message}`,
 			},
 			{
 				status: 500,
